f35101bb955431a158938b0054026a31be05937e
Alecu Ștefan-Iulian <96354838+stalecu@users.noreply.github.com>
2023-10-03 00:10:38 +0300

    [0024] Oglindit2

---
+++ 0024-Oglindit2/generate_testcases.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+use List::Util qw/sum/;
+my $N = 2**29 - 1; # Nu stiu de ce orice exponent peste 29 nu merge...
+my $M = 2**15 - 1;
+
+open my $fh, '>', 'expected.txt' or die $!;
+open my $hf, '>', 'input.txt' or die $!;
+
+for my $i (2..$M) {
+    my $random = int(1 + rand($N));
+    print $hf "$random\n";    
+    my $rev = int reverse "$random";
+    print $fh "$rev\n";
+}
